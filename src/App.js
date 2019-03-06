0b6336f9b465c80c5b5e370b48f5bf737cec991b
Ferveloper <fmerino@clavijo.lan>
2019-03-06 10:37:34 +0100

    Basic functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import marked from 'marked'
 
 class App extends Component {
+  state = {
+    text : ``,
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <textarea name="text" id="editor" cols="30" rows="10" onChange={this.handleChange}></textarea>
+        <div id="preview" dangerouslySetInnerHTML={{__html: marked(this.state.text)}}/>
       </div>
     );
   }
+  
+  handleChange = (e) => this.setState({text : e.target.value})
 }
 
 export default App;
